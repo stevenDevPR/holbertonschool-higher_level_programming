a9b98f3fb1eabff14f248100dd4325f0e50081b9
stevendevpr <stevenbonilla46@yahoo.com>
2024-05-10 22:53:25 -0400

    task 6

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-language that combines remarkable power with very clear syntax"
-print(str[str.find("object-oriented"):str.find("object-oriented") + 
-          len("object-oriented")])
+        language that combines remarkable power with very clear syntax"
+str = str.split()
+print(f"{str[5]} {str[6]} {str[12]} {str[0]}")
